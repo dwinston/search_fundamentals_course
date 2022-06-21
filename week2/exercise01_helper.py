d8bafc6248a0390183c5c82fc10200513bbed03f
Donny Winston <donny@polyneme.xyz>
2022-06-21 10:49:23 -0400

    w2 project: update indexing specs

---
+++ week2/exercise01_helper.py
@@ -0,0 +1,15 @@
+[
+  {
+    "query": "lcd tv",
+    "assumed_intent": "looking to buy an lcd tv",
+    "results": [
+      {
+        "no":  1,
+        "id": "1099389620562",
+        "name": "Magnavox 15\" LCD TV",
+        "relevant": true,
+
+      }
+    ]
+  }
+]
